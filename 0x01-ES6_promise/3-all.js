be0f1bd171a94c963b22d99f7bc85b4eef9261d9
martinsndifon <martinsndifon@gmail.com>
2023-06-27 21:29:56 +0100

    update to task-3, promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(([photoResponse, userResponse]) => {
     console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
-  });
+  }).catch(() => console.log('Signup system offline'));
 }
