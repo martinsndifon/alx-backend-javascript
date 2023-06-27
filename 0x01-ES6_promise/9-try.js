c74d70a878cdb858a0b2a8043578893f8518f86c
martinsndifon <martinsndifon@gmail.com>
2023-06-27 23:06:23 +0100

    update to task-9, try and catch

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,10 +3,10 @@ export default function guardrail(mathFunction) {
   try {
     const res = mathFunction();
     queue.push(res);
-    queue.push('Guardrail was processed');
   } catch (e) {
     const errorMessage = `Error: ${e.message}`;
     queue.push(errorMessage);
+  } finally {
     queue.push('Guardrail was processed');
   }
   return queue;
