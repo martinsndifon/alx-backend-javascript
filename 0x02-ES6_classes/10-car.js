b4cceeac525bad8f12e439189aaf718641f978f3
martinsndifon <martinsndifon@gmail.com>
2023-06-29 22:04:56 +0100

    update to task-10, more Symbol

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -9,6 +9,30 @@ export default class Car {
     this._color = color;
   }
 
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
   static get [Symbol.species]() {
     return this;
   }
