bfbbfed52078f06d5032a93221ebec99fefeb7aa
martinsndifon <martinsndifon@gmail.com>
2023-06-29 18:21:12 +0100

    update to task-2, setters and getters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,60 +1,59 @@
-/* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw new Error('Name must be a string');
+      throw TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
-      throw new Error('Length must be a number');
+      throw TypeError('Length must be a number');
     }
     if (typeof students !== 'object') {
-      throw TypeError('students must be an array of strings');
+      throw TypeError('students must be array of strings');
     }
     for (const val of students) {
       if (typeof val !== 'string') {
-        throw TypeError('students must be an array of strings');
+        throw TypeError('students must be array of strings');
       }
     }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this._name = name; // eslint-disable-line
+    this._length = length; // eslint-disable-line
+    this._students = students; // eslint-disable-line
   }
 
   get name() {
-    return this._name;
+    return this._name; // eslint-disable-line
   }
 
   set name(name) {
     if (typeof name !== 'string') {
-      throw new Error('Name must be a string');
+      throw TypeError('Name must be a string');
     }
-    this._name = name;
+    this._name = name; // eslint-disable-line
   }
 
   get length() {
-    return this._name;
+    return this._length; // eslint-disable-line
   }
 
   set length(length) {
     if (typeof length !== 'number') {
-      throw new Error('Length must be a number');
+      throw TypeError('Length must be a number');
     }
-    this._length = length;
-  }
-
-  get students() {
-    return this._students;
+    this._length = length; // eslint-disable-line
   }
 
   set students(students) {
     if (typeof students !== 'object') {
-      throw TypeError('students must be an array of strings');
+      throw TypeError('students must be array of strings');
     }
     for (const val of students) {
       if (typeof val !== 'string') {
-        throw TypeError('students must be an array of strings');
+        throw TypeError('students must be array of strings');
       }
     }
-    this._students = students;
+    this._students = students; // eslint-disable-line
+  }
+
+  get students() {
+    return this._students; // eslint-disable-line
   }
 }
