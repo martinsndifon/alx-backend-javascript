fbe0e11ef2667eecb62e8dbae4932339317bc79f
martinsndifon <martinsndifon@gmail.com>
2023-06-29 18:24:01 +0100

    update to task-2, setters and getters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
@@ -14,31 +15,31 @@ export default class HolbertonCourse {
         throw TypeError('students must be array of strings');
       }
     }
-    this._name = name; // eslint-disable-line
-    this._length = length; // eslint-disable-line
-    this._students = students; // eslint-disable-line
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
 
   get name() {
-    return this._name; // eslint-disable-line
+    return this._name;
   }
 
   set name(name) {
     if (typeof name !== 'string') {
       throw TypeError('Name must be a string');
     }
-    this._name = name; // eslint-disable-line
+    this._name = name;
   }
 
   get length() {
-    return this._length; // eslint-disable-line
+    return this._length;
   }
 
   set length(length) {
     if (typeof length !== 'number') {
       throw TypeError('Length must be a number');
     }
-    this._length = length; // eslint-disable-line
+    this._length = length;
   }
 
   set students(students) {
@@ -50,10 +51,10 @@ export default class HolbertonCourse {
         throw TypeError('students must be array of strings');
       }
     }
-    this._students = students; // eslint-disable-line
+    this._students = students;
   }
 
   get students() {
-    return this._students; // eslint-disable-line
+    return this._students;
   }
 }
