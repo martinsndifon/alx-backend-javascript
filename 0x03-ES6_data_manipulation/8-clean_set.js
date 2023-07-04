7d91cbb53223ed2073c5f708924dbe65d2e39812
martinsndifon <martinsndifon@gmail.com>
2023-07-04 13:03:56 +0100

    update to task-8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (!(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
   const arr = [];
