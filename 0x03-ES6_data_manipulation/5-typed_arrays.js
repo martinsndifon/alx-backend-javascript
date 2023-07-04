5a09371179de4d0141b798930643af7104343d4f
martinsndifon <martinsndifon@gmail.com>
2023-07-04 11:59:14 +0100

    updated the function to create int8 using DataView

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position > (length - 1)) {
+  if (position >= length) {
     throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
-  const int8view = new Int8Array(buffer);
-  int8view[position] = value;
+  const view = new DataView(buffer, 0, length);
+  view.setInt8(position, value);
 
-  return buffer;
+  return view;
 }
