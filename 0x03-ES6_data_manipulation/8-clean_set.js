f559db31df30ecbe04d5d34dcfcf5a5c0563dc6f
martinsndifon <martinsndifon@gmail.com>
2023-07-04 13:13:47 +0100

    update to task-8, string manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@ export default function cleanSet(set, startString) {
   }
   const arr = [];
   for (const elem of set) {
-    if (elem.startsWith(startString)) {
+    if (typeof elem === 'string' && elem.startsWith(startString)) {
       arr.push(elem.slice(startString.length));
     }
   }
