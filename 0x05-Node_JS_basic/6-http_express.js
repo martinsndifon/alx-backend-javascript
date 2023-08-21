b81c937bb128b5bfb0dd4b4afcfaa5011d860c06
martinsndifon <martinsndifon@gmail.com>
2023-08-21 23:46:25 +0100

    simple express server

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,9 +4,13 @@ const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+module.exports = app;
