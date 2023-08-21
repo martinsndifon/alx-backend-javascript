fdea8515e00436878abfa8e255ca8d73123b820d
martinsndifon <martinsndifon@gmail.com>
2023-08-21 21:41:32 +0100

    update to task-2, synchronously read from file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,41 +3,27 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const newdata = data.split('\n');
-    newdata.shift();
-    console.log(`Number of students: ${newdata.length}`);
-    let cs = 0;
-    const csStudents = [];
-    let swe = 0;
-    const sweStudents = [];
-    newdata.forEach((student) => {
-      const details = student.split(',');
-      if (details[3] === 'CS') {
-        cs += 1;
-        csStudents.push(student);
-      } else if (details[3] === 'SWE') {
-        swe += 1;
-        sweStudents.push(student);
+    const dataList = data.split('\n').splice(1);
+    const stats = { CS: [], SWE: [] };
+    let students = 0;
+    for (const line of dataList) {
+      const columns = line.split(',');
+      if (columns[3] === 'CS') {
+        stats.CS.push(columns[0]);
+        students += 1;
+      } else if (columns[3] === 'SWE') {
+        stats.SWE.push(columns[0]);
+        students += 1;
       }
-    });
-    const csFirstNames = [];
-    for (const names of csStudents) {
-      const data = names.split(',');
-      csFirstNames.push(data[0]);
     }
-    const sweFirstNames = [];
-    for (const names of sweStudents) {
-      const data = names.split(',');
-      sweFirstNames.push(data[0]);
+    console.log(`Number of students: ${students}`);
+    for (const [key, value] of Object.entries(stats)) {
+      console.log(
+        `Number of students in ${key}: ${value.length}. List: ${value.join(
+          ', '
+        )}`
+      );
     }
-    console.log(
-      // eslint-disable-next-line comma-dangle
-      `Number of students in CS: ${cs}. List: ${csFirstNames.join(', ')}`
-    );
-    console.log(
-      // eslint-disable-next-line comma-dangle
-      `Number of students in SWE: ${swe}. List: ${sweFirstNames.join(', ')},`
-    );
   } catch (err) {
     if (err.code === 'ENOENT') {
       throw new Error('Cannot load the database');
