c4b2058d1b8b5ff7ca97487c4ed6d927be889079
martinsndifon <martinsndifon@gmail.com>
2023-08-21 22:24:57 +0100

    A small http server using the http module of nodejs

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,9 +3,10 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School');
+  res.end('Hello Holberton School!');
 });
 
+// eslint-disable-next-line jest/require-hook
 app.listen(1245, '127.0.0.1', () => console.log('Server running'));
 
 module.exports = app;
