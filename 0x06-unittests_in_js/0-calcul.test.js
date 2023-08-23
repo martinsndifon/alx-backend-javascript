94a290c0cf48eddc58ad9026d0eff3b6c7ac8f1a
martinsndifon <martinsndifon@gmail.com>
2023-08-23 19:04:32 +0100

    Adjust mocha test with 2 describes

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,15 +2,17 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('adds two rounded numbers correctly', () => {
-    assert.strictEqual(calculateNumber(3, 5), 8);
-  });
+  describe('add function', () => {
+    it('adds two rounded numbers correctly', () => {
+      assert.strictEqual(calculateNumber(3, 5), 8);
+    });
 
-  it('adds two rounded numbers correctly', () => {
-    assert.strictEqual(calculateNumber(3.7, 1), 5);
-  });
+    it('adds two rounded numbers correctly', () => {
+      assert.strictEqual(calculateNumber(3.7, 1), 5);
+    });
 
-  it('adds two rounded numbers correctly', () => {
-    assert.strictEqual(calculateNumber(3.7, 5.2), 9);
+    it('adds two rounded numbers correctly', () => {
+      assert.strictEqual(calculateNumber(3.7, 5.2), 9);
+    });
   });
 });
