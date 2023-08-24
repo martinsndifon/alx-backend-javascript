99f042c238caad9930662f2b545877986de043d7
martinsndifon <martinsndifon@gmail.com>
2023-08-24 18:48:41 +0100

    test using assert instead of chai

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,6 +1,6 @@
 const sinon = require('sinon');
 const chai = require('chai');
-//const assert = require('assert');
+const assert = require('assert');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
@@ -20,10 +20,8 @@ describe('sendPaymentRequestToApi', () => {
     sendPaymentRequestToApi(100, 20);
 
     // Use Chai's expect assertions to verify the spy
-    expect(calculateNumberStub.calledOnce).to.be.true;
-    expect(calculateNumberStub.firstCall.args).to.deep.equal(['SUM', 100, 20]);
-    expect(consoleLogSpy.calledOnce).to.be.true;
-    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+    assert(consoleLogSpy.withArgs('The total is: 10').calledOnce);
+    assert(calculateNumberStub.withArgs('SUM', 100, 20).calledOnce);
 
     // Restore the original function after the test
     calculateNumberStub.restore();
