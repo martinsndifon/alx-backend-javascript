85c9d099f014fe8fb76c49eb61fc6746e565e88a
martinsndifon <martinsndifon@gmail.com>
2023-08-24 09:13:35 +0100

    correction made to sinon.stub function

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -8,7 +8,7 @@ const expect = chai.expect;
 
 describe('sendPaymentRequestToApi', () => {
   it('should call Utils.calculateNumber with correct arguments', () => {
-    // Create a spy for Utils.calculateNumber
+    // Create a stub for Utils.calculateNumber
     const calculateNumberStub = sinon
       .stub(Utils, 'calculateNumber')
       .returns(10);
@@ -18,9 +18,6 @@ describe('sendPaymentRequestToApi', () => {
 
     // Call the function you want to test
     sendPaymentRequestToApi(100, 20);
-
-    // Assert that Utils.calculateNumber was called correctly
-    //assert(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20));
 
     // Use Chai's expect assertions to verify the spy
     expect(calculateNumberStub.calledOnce).to.be.true;
