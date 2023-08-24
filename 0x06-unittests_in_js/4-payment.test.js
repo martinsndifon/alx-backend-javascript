af9600b9023ad14df8d6b9d2513e2d574b5c23c6
martinsndifon <martinsndifon@gmail.com>
2023-08-24 08:53:48 +0100

    Using stub and spy of sinon test utility

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,35 @@
+const sinon = require('sinon');
+const chai = require('chai');
+//const assert = require('assert');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+const expect = chai.expect;
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    // Create a spy for Utils.calculateNumber
+    const calculateNumberStub = sinon
+      .stub(Utils, 'calculateNumber')
+      .returns(10);
+
+    // Add a spy to verify the return of sendPaymentRequestToApi
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    // Call the function you want to test
+    sendPaymentRequestToApi(100, 20);
+
+    // Assert that Utils.calculateNumber was called correctly
+    //assert(calculateNumberSpy.calledOnceWithExactly('SUM', 100, 20));
+
+    // Use Chai's expect assertions to verify the spy
+    expect(calculateNumberStub.calledOnce).to.be.true;
+    expect(calculateNumberStub.firstCall.args).to.deep.equal(['SUM', 100, 20]);
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+
+    // Restore the original function after the test
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+});
